4fcf793698986808170069c717580e1c0694ac5c
lluke3871 <lluke3871@gmail.com>
2025-09-11 11:25:21 +0900

    little edit in ui.main2

--- ui/main.py
+++ ui/main.py
@@ -1,5 +1,5 @@
 import sys, os
-sys.path.append(os.path.dirname(__file__))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import streamlit as st
 from tab1_daiso_section import render_daiso_section
